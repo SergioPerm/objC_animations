a310df3468705e0fa4705e0e076b3b1ae5a30d0a
Admin <admin@mac-admin.local>
2018-06-27 12:55:17 +0300

    Initial Commit

---
+++ AnimationsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationsTest
+//
+//  Created by Admin on 27.06.2018.
+//  Copyright © 2018 Sergio Lechini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationsTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AnimationsTest
+//
+//  Created by Admin on 27.06.2018.
+//  Copyright © 2018 Sergio Lechini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
