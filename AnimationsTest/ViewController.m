5f8f587df30ba0180765e4f589b9bd9a408eb240
Сергей Лепинин <sergioprm@icloud.com>
2019-03-14 05:13:59 -0700

    123

--- AnimationsTest/ViewController.m
+++ AnimationsTest/ViewController.m
@@ -10,8 +10,8 @@
 
 @interface ViewController ()
 
-@property (weak, nonatomic) UIView* testView;
-@property (weak, nonatomic) UIView* testView2;
+//@property (weak, nonatomic) UIView* testView;
+@property (weak, nonatomic) UIImageView* testImageView;
 
 @end
 
@@ -21,18 +21,25 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    UIView* view = [[UIView alloc] initWithFrame:CGRectMake(10, 10, 80, 80)];
-    view.backgroundColor = [UIColor greenColor];
+    UIImageView* view = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, 40, 40)];
+    
+    UIImage* image1 = [UIImage imageWithContentsOfFile:@"/Users/sergejlepinin/Documents/iOSeducation/objC/objC_uiviewAnim/AnimationsTest/images/1.jpg"];
+    UIImage* image2 = [UIImage imageWithContentsOfFile:@"/Users/sergejlepinin/Documents/iOSeducation/objC/objC_uiviewAnim/AnimationsTest/images/2.jpg"];
+    UIImage* image3 = [UIImage imageWithContentsOfFile:@"/Users/sergejlepinin/Documents/iOSeducation/objC/objC_uiviewAnim/AnimationsTest/images/3.jpg"];
+    
+    NSArray* images = [NSArray arrayWithObjects:image1, image2, image3, image1, nil];
+    
+    view.animationImages = images;
+    view.animationDuration = 1.0f;
+    [view startAnimating];
+    
+    //view.backgroundColor = [UIColor greenColor];
     
     [self.view addSubview:view];
     
-    UIView* view2 = [[UIView alloc] initWithFrame:CGRectMake(30, 200, 40, 40)];
-    view2.backgroundColor = [UIColor blueColor];
     
-    [self.view addSubview:view2];
+    self.testImageView = view;
     
-    self.testView = view;
-    self.testView2 = view2;
 }
 
 - (void)viewDidAppear:(BOOL)animated {
@@ -44,8 +51,8 @@
 //                     }];
     
 
-    [self moveView:self.testView:5];
-    [self moveView:self.testView2:1];
+    [self moveView:self.testImageView:0.5];
+    //[self moveView:self.testView2:1];
     
 //    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
 //        [self.testView.layer removeAllAnimations];
@@ -61,23 +68,33 @@
     rect = CGRectInset(rect, CGRectGetWidth(view.frame), CGRectGetHeight(view.frame));
     
     CGFloat x = arc4random() % (int)CGRectGetWidth(rect) + CGRectGetMinX(rect);
+    CGFloat y = arc4random() % (int)CGRectGetHeight(rect) + CGRectGetMinY(rect);
+    
+    CGFloat rndScale = .1f + (float)(arc4random() % 151) / 100.f + 0.5f;
+    
+    NSLog(@"scale: %f", rndScale);
+    
+    CGFloat rndRotate = (float)(arc4random() % (int)(M_PI * 2 * 10000)) / 10000 - M_PI;
     
     [UIView animateWithDuration:dur
                           delay:0
                         options:UIViewAnimationOptionCurveLinear
                      animations:^{
-                         view.center = CGPointMake(300, 200);
-                         view.backgroundColor = [self randomColor];
+                         view.center = CGPointMake(x, y);
+                         //view.backgroundColor = [self randomColor];
                          
-                         CGAffineTransform scale = CGAffineTransformMakeScale(2, 2);
-                         CGAffineTransform rotation = CGAffineTransformMakeRotation(M_PI);
+                         CGAffineTransform scale = CGAffineTransformMakeScale(rndScale, rndScale);
+                         CGAffineTransform rotation = CGAffineTransformMakeRotation(rndRotate);
                          
                          CGAffineTransform transform = CGAffineTransformConcat(scale, rotation);
                          
                          view.transform = transform;
                      }
                      completion:^(BOOL finished) {
-                         view.backgroundColor = [UIColor redColor];
+                         
+                         __weak UIView* v = view;
+                        
+                         [self moveView:v :1];
                      }];
     
 }
